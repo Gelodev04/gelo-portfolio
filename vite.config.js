00743f163d30bf9bf381aa25af927574ade59d4b
gelo <gelodev83@gmail.com>
2025-01-14 20:00:03 +0800

    optimized

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteImagemin from 'vite-plugin-imagemin';
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    minify: "terser", // Minify using Terser (for JavaScript)
+    cssCodeSplit: true, // Split CSS into separate files
+  },
+  plugins: [react(),viteImagemin({})],
+});
